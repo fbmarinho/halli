8e167a3e6249deab95d986dd5571abfb3e5c948a
Felipe Marinho <felipegigamail@gmail.com>
2021-09-21 17:13:42 -0300

    Update card.js

--- pages/api/card.js
+++ pages/api/card.js
@@ -1,10 +1,10 @@
-export default () => {
-  return JSON.Stringify({
+export default function handler(req, res) {
+  const date = new Date(Date.now());
+  res.status(200).json({
     date: new Date(Date.now()),
     card: {
       subject: "EPI inadequado",
       observation: "Observei operador sem luvas adequadas para o trabalho",
       action: "Alertei ao mesmo que providenciou as luvas"
-    }
-  });
+    }});
 }
